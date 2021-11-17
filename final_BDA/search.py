3d949263bc4b0bade5b46b2beb02ecdf0bc85b52
Edward Zhou <edwardzhou0630@gmail.com>
2021-11-16 20:45:30 -0500

    push to gbq when loading /results

--- final_BDA/search.py
+++ final_BDA/search.py
@@ -3,12 +3,16 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 
+import os
+from google.cloud import bigquery
+
 # 接收POST请求数据
 def search_post(request):
     '''
     please upload the data to DB there, and get the result
     '''
     original_data= {}
+    print('searching')
     if request.POST:
         '''
         print(request.POST)
@@ -34,7 +38,7 @@ def search_post(request):
         'Same Gender': ['Yes'],
         'Time': ['Early Bird']}>
         '''
-
+        print('*******************************8post*************8')
         original_data['roommatesRecommendation'] = "Roommates Recommendation"
         original_data['apartmentRecommendation'] = "Apartment Recommendation"
         original_data['firstName'] = request.POST.getlist('First Name')
@@ -56,6 +60,8 @@ def search_post(request):
         original_data['sameMajor'] = request.POST.getlist('Same Major')
         original_data['sameGender'] = request.POST.getlist('Same Gender')
         original_data['time'] = request.POST.getlist('Time')
+
+    print(original_data)
     show_results(request)
     return render(request, "post.html", original_data)
 
@@ -64,6 +70,7 @@ def show_results(request):
     '''
     please put the result there and I can show them on the result page
     '''
+    print(f'request: {request.POST}')
     results_data= {}
     if request.POST:
         results_data['roommatesRecommendation'] = "Roommates Recommendation"
@@ -86,6 +93,49 @@ def show_results(request):
         results_data['gender'] = request.POST['Gender']
         results_data['nationality'] = request.POST['Nationality']
         results_data['email'] = request.POST['Email']
-#         results_data['apartment'] = request.POST.getlist('Apartment')
+        # results_data['apartment'] = request.POST.getlist('Apartment')
         results_data['roomType'] = str(request.POST.getlist('Room Type')).replace('[','').replace(']','').replace("'",'')
+
+        push_to_gbq(results_data)
     return render(request, 'results.html', results_data)
+
+
+def push_to_gbq(results_data):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/edz/.ssh/big-data-6893-326823-15d1e60fd014.json"
+    client = bigquery.Client()
+    table_id = "big-data-6893-326823.roommate.users"
+
+
+    rows_to_insert = [
+        {
+        'roommatesRecommendation': results_data['roommatesRecommendation'],
+        'apartmentRecommendation': results_data['apartmentRecommendation'],
+        'firstName': results_data['roommateRatio'],
+        'lastName': results_data['apartmentRatio'],
+        'uni': results_data['location'],
+        'gender': results_data['contact'],
+        'nationality': results_data['apartment'],
+        'email': results_data['apartmentRatio1'],
+        'school': results_data['location1'],
+        'major': results_data['contact1'],
+        'smoking': results_data['apartment1'],
+        'alcohol': results_data['roomType1'],
+        'certainApartment': results_data['firstName'],
+        'apartment': results_data['lastName'],
+        'distance': results_data['fullName'],
+        'roomType': results_data['gender'],
+        'roommate': results_data['nationality'],
+        'number': results_data['email'],
+        'sameMajor': results_data['apartment'],
+        'sameGender': results_data['roomType'],
+        'time': 'now'
+        }
+    ]
+    errors = client.insert_rows_json(
+        table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert)
+    )  # Make an API request.
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
